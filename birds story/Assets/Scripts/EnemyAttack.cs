6005d035bc3e1933856fce8f3130ccd983540cb7
CptDrusha <rainbowdashsteam@gmail.com>
2019-01-27 04:20:14 +0300

    EnemyController + Birds Falling Down

---
+++ birds story/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+
+    private bool looking;
+    public bool inverse;
+
+    private GameObject player;
+
+    public int timer = 5;
+    private float time;
+
+    private Rigidbody2D rb2;
+    public float boostSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        looking = true;
+        time = timer;
+
+        rb2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log(player.transform.position);
+
+        time -= Time.deltaTime;
+
+        if (looking)
+        {
+            Vector3 vectorToTarget = player.transform.position - transform.position;
+            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime);
+        } 
+
+        if(time < 0)
+        {
+            looking = false;
+            transform.position += transform.right * Time.deltaTime * boostSpeed;
+        }
+
+        if(time < -timer * 3)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ birds story/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public GameObject player;
+
+    [Range(0.01f, 4f)]
+    public float diff = 1f;
+
+    public int timer = 10;
+    private float time;
+
+    private BoxCollider2D coll;
+
+    private bool lookingTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookingTarget = true;
+        time = timer;
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    private Transform pos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time < 0)
+        {
+            time = timer;
+            var bounds = coll.bounds;
+            var center = bounds.center;
+            var x = UnityEngine.Random.Range(center.x - bounds.extents.x, center.x + bounds.extents.x);
+            var y = UnityEngine.Random.Range(center.y - bounds.extents.y, center.y + bounds.extents.y);
+
+            Vector2 posEnemy = new Vector2(x, y);
+            GameObject enemyBird;
+            enemyBird = Instantiate(enemy, new Vector2(x,y), Quaternion.identity);
+
+            if (posEnemy.x > player.transform.position.x)
+            {
+                var scale = enemyBird.transform.localScale;
+                enemyBird.transform.localScale = new Vector3(scale.x, Mathf.Abs(scale.y) * -1, scale.z);
+
+                enemyBird.GetComponent<EnemyAttack>().inverse = true;
+            }
+
+            Attack(enemyBird);
+        }
+        time -= Time.deltaTime * diff;
+    }
+
+    void Moving(GameObject enemy)
+    {
+
+    }
+
+    void Attack(GameObject enemy)
+    {
+        if (lookingTarget)
+        {
+            
+        }
+    }
+}
